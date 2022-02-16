377c2e1cf537f6477e08a6c6c0dd70847faacc54
Bram Borggreve <borggreve@gmail.com>
2022-02-16 16:46:54 -0500

    fix: expose validateKinTransaction method

--- packages/kin-transaction/src/index.ts
+++ packages/kin-transaction/src/index.ts
@@ -1,3 +1,5 @@
-export * from './lib/parse-kin-transaction';
-export * from './lib/kin-transaction-constants';
 export * from './lib/kin-parsed-transaction';
+export * from './lib/kin-transaction-constants';
+export * from './lib/kin-validate-transaction';
+export * from './lib/parse-kin-transaction';
+export * from './lib/validate-kin-transaction';
