c718f9b2ada92563c147bc2f31003b96009076ed
Bram Borggreve <borggreve@gmail.com>
2022-03-18 16:07:28 -0500

    feat: remove circulating package

--- packages/circulating-supply/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/circulating-supply';
-export { CirculatingSupplyConfig } from './lib/circulating-supply.config';
--- packages/circulating-supply/src/lib/kin/get-public-key.ts
+++
@@ -1,9 +0,0 @@
-import { PublicKey } from '@kinecosystem/kin-sdk-v2';
-
-export function getPublicKey(publicKey: string): PublicKey {
-  try {
-    return PublicKey.fromBase58(publicKey);
-  } catch (_) {
-    return PublicKey.fromString(publicKey);
-  }
-}
